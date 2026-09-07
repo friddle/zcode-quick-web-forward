f32869c7f5b4378a2f56d94ea363186f535ef170
friddle-copper <friddle.l@code27.co>
2026-09-07 23:39:41 +0800

    chore: gitignore official-host/host-deploy build output

--- official-host/host-deploy/host/chunk-AZEX74CX.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./chunk-JVYS45FK.js";import"./chunk-FX4G3PHU.js";import"./chunk-KGXW6KHC.js";export{a as DockerBackend};
--- official-host/host-deploy/host/chunk-WRT6QIHJ.js
+++
@@ -1,1 +0,0 @@
-import{d as a,e as b}from"./chunk-VLSSTGV6.js";import"./chunk-FX4G3PHU.js";import"./chunk-KGXW6KHC.js";export{b as WSLBackend,a as buildWslArgs};
